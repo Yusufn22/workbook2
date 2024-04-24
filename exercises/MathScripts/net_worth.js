c747ed786c4fb7193fbe175396bf48eba2aa7487
Yusufn22 <Yusufnaderi24@gmail.com>
2024-04-24 11:14:18 -0400

    New changes

---
+++ exercises/MathScripts/net_worth.js
@@ -0,0 +1,4 @@
+var netWorth = "some-number"
+var debts = 500000;
+var netWorth = assets - debts
+console.log( "Your networth is " + netWorth )
---
+++ scratchpad.js
@@ -0,0 +1,6 @@
+var firstName = "Yusuf";
+console.log("Hi, my name is " + firstName)
+
+var cityName = "Queens";
+var stateName= "New York"
+console.log(" Hi my name is " + firstName + " I was born in " + cityName + " that is located in "+ stateName)
