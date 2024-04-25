bae5852d1b7c9c20ffc2d89e7fadbd77f9bdc981
Yusufn22 <Yusufnaderi24@gmail.com>
2024-04-25 12:53:01 -0400

    changes

--- exercises/pay_rules.js
+++ exercises/pay_rules.js
@@ -1,8 +1,15 @@
-var payRate = 15;
-var hoursWorked = 30;
-grossPay = payRate * hoursWorked;
-if (hoursWorked >40) {
-grossPay = grossPay * 1.5;
+var regularPayRate = 25;
+var hoursWorked = 40;
+var regularHours = 40;
+var overtimeHours = hoursWorked - regularHours;
+var overtimePayRate = regularPayRate * 1.5;
+overtimepay = overtimePayRate * overtimeHours;
+var grossPay;
+if (hoursWorked<=40) {
+    grossPay = regularPayRate * hoursWorked;
     
+} else{
+    grossPay = regularPayRate * regularHours * overtimePay;
+
 }
- console.log(grossPay);
+console.log(grossPay);
