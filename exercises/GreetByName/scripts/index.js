9f8ffb89412875d8cc1d653899ad9f15bd5a51e1
Yusufn22 <Yusufnaderi24@gmail.com>
2024-04-26 15:10:39 -0400

    exercises

---
+++ exercises/GreetByName/scripts/index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+    window.onload = init;
+    function init() {
+        const helloBtn = document.getElementById("greetBtn");
+        helloBtn.onclick = onHelloBtnClicked;
+      }
+
+
+
+    function onHelloBtnClicked() {
+        alert("Hello");
+      }
---
+++ exercises/first_function.js
@@ -0,0 +1,9 @@
+let message = "Hello World";
+console.log(message);
+
+
+function greet(){
+let message = "Hello World";
+console.log(message)
+
+}
